85fd96fe1012e26603876586fbee1794235bf515
Jin Cheng <jincheng411@gmail.com>
2024-09-11 17:05:49 +0300

    add functions for substraction multiplication and division also add tests

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,2 +1,16 @@
+'''This is the implementaion for calculator'''
 def add(a, b):
-  return a + b
+    '''add function for calculator'''
+    return a + b
+
+def substract(a, b):
+    '''substraction function for calculator'''
+    return a - b
+
+def multiply(a, b):
+    '''multiplication function for calculator'''
+    return a * b
+
+def divide(a, b):
+    '''division function for calculator'''
+    return a / b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,17 @@
-from calculator import add
+'''test cases for calculator'''
+from calculator import add, substract, multiply, divide
 def test_add():
-  assert add(2, 2) == 4
+    '''test addition function'''
+    assert add(2, 2) == 4
+
+def test_substraction():
+    '''test subsraction function'''
+    assert substract(5, 2) == 3
+
+def test_multiplication():
+    '''test multiplication function'''
+    assert multiply(2, 4) == 8
+
+def test_division():
+    '''test division function'''
+    assert divide(10, 2) == 5
